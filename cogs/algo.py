a9a400fced85da3b12a898794eb732918d823027
CheeMinHao <cheemh3456@gmail.com>
2021-07-31 20:28:13 +0800

    feat(Embed): Created Embed Functions for Bot

--- cogs/algo.py
+++ cogs/algo.py
@@ -8,14 +8,92 @@ class Algorithm(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
+        """
+        Checks for Ping for bot
+        """
         await ctx.send(f'Pong! {round(self.client.latency * 1000)}ms')
 
-    @commands.command(aliases = ['algo', 'algorithm'])
+    @commands.command(aliases = ['algo', 'algorithm', 'algos'])
     async def _algoType(self, ctx, *, request):
+        """
+        Checks if Algorithm has a Sub Type and sends respective embed
+        """
+        # Loads Json File
         f = open('data.json',)
         data = json.load(f)
-        for i in range(len(data)):
-            await ctx.send(data[i])
+
+        # Separating Algorithms with their presence of subtypes
+        algo_types = ['searching']
+        algo_sub_types = ['sorting']
+
+        # Checks if Algorithm is a Sub Type
+        if request.lower() in algo_types:
+
+            # if Algorithm has no sub types, sets appropriate json hierachy
+            for i in range(len(data)):
+                if data[i]['algo_type'].lower() == request.lower():
+
+                    # Sets Algorithm
+                    algorithm = data[i]['algos'][0]
+
+                    # Calls Embed Creation Function
+                    await ctx.invoke(self.client.get_command('algoSend2'), algorithm=algorithm)
+
+        elif request.lower() in algo_sub_types:
+
+            # if Algorithm has sub types, sets appropriate json hierachy
+            for i in range(len(data)):
+                if data[i]['algo_type'].lower() == request.lower():
+
+                    # Sets Algorithm Sub Type and Algorithm
+                    algorithm = data[i]['algo_subtype'][0]['algos'][0]
+                    sub_name = data[i]['algo_subtype'][0]['algo_subtype_name']
+
+                    # Calls Embed Creation Function
+                    await ctx.invoke(self.client.get_command('algoSend'), algorithm=algorithm, sub_name=sub_name)
+
+
+    @commands.command()
+    async def algoSend(self, ctx, algorithm: object, sub_name: str):
+        """
+        Creates Embed for Algorithm with Sub Type
+        """
+        # Sets variables
+        title = algorithm['algo_name']
+        span = algorithm['algo_name']
+
+        # Sets Description
+        description = ""
+        for i in range(len(algorithm['algo_desc'])):
+            description += algorithm['algo_desc'][i] + "\n\n"
+
+        # Creates Embed
+        algoEmbed = discord.Embed(title=title, description=sub_name, color=0x00ff00)
+        algoEmbed.add_field(name="Description\n", value=description, inline=False)
+
+        # Sends Embed
+        await ctx.channel.send(embed=algoEmbed)
+
+    @commands.command()
+    async def algoSend2(self, ctx, algorithm: object):
+        """
+        Creates Embed for Algorithm without Sub Type
+        """
+        # Sets variables
+        title = algorithm['algo_name']
+        span = algorithm['algo_name']
+
+        # Sets Description
+        description = ""
+        for i in range(len(algorithm['algo_desc'])):
+            description += algorithm['algo_desc'][i] + "\n\n"
+
+        # Creates Embed
+        algoEmbed = discord.Embed(title=title, description=title, color=0x00ff00)
+        algoEmbed.add_field(name="Description\n", value=description, inline=False)
+
+        # Sends Embed
+        await ctx.channel.send(embed=algoEmbed)
 
 def setup(client):
     client.add_cog(Algorithm(client))
--- main.py
+++ main.py
@@ -5,29 +5,43 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Prefix for Bot
 client = commands.Bot(command_prefix="$")
 
 @client.event
 async def on_ready():
+    """
+    Initiates Bot
+    """
     await client.change_presence(status=discord.Status.idle, activity=discord.Game('My Academic Results'))
     print('Bot is ready')
 
 @client.command()
 async def load(ctx, extension):
+    """
+    Loads Cogs
+    """
     client.load_extension(f'cogs.{extension}')
 
 @client.command()
 async def unload(ctx, extension):
+    """
+    Unloads Cogs
+    """
     client.unload_extension(f'cogs.{extension}')
 
 @client.command()
 async def reload(ctx, extension):
+    """
+    Reloads Cogs
+    """
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
 
+# Initialise All Cog Files
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-
+# Runs Client depending on Client Token
 client.run(os.getenv('TOKEN'))
